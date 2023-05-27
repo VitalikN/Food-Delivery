2b9d62e1a912884e3ae5c9b368d047c5380da2a3
vitalik.nozhenko@gmail.com <vitalik.nozhenko@gmail.com>
2023-05-27 21:31:20 +0300

    git

--- config.js
+++
@@ -1,8 +0,0 @@
-const SECRET_KEY = "Ad8WtrhM3MDrKKvA";
-const DB_HOST =
-  "mongodb+srv://Vitalii:Ad8WtrhM3MDrKKvA@cluster0.ietjxtl.mongodb.net/food-delivery?retryWrites=true&w=majority";
-
-module.exports = {
-  SECRET_KEY,
-  DB_HOST,
-};
