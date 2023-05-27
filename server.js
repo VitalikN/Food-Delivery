7079e23e78eca9b4dfd73d8f645324bf841d668e
vitalik.nozhenko@gmail.com <vitalik.nozhenko@gmail.com>
2023-05-27 22:02:08 +0300

    DB_HOST

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const { DB_HOST } = require("./config");
+// const { DB_HOST } = require("./config");
+const DB_HOST =
+  "mongodb+srv://Vitalii:Ad8WtrhM3MDrKKvA@cluster0.ietjxtl.mongodb.net/food-delivery?retryWrites=true&w=majority";
 
 const app = require("./app");
 
